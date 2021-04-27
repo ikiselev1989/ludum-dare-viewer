70541261afd6c363e2aed1e8d4888e7ca5cdd91c
Ilya Kiselev <ikiselev1989@gmail.com>
2021-04-27 19:05:33 +0300

    ym

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import VueYandexMetrika from 'vue-yandex-metrika'
 Vue.config.productionTip = false
 
 Vue.use(VueYandexMetrika, {
-    id: '244ee7430ab9a70f',
+    id: '76681307',
     env: process.env.NODE_ENV
 })
 
